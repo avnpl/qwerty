1156cae2940238342819e749928b4690e2d9b367
avnpl <alvinpaul1311@gmail.com>
2023-02-23 20:48:28 +0530

    🚧 Added morgan and setup routers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 import express from 'express'
+import morgan from 'morgan'
+import { router } from './routes'
 
-const app = express()
 const prisma = new PrismaClient()
+const app = express()
+const port = process.env.PORT
 
 app.use(express.json())
+app.use(router)
+app.use(morgan('dev'))
 
-const server = app.listen(8080, () => {
-  console.log('Server ready at http://localhost:8080')
+const server = app.listen(port, () => {
+  console.log(`Server ready at http://localhost:${port}`)
 })

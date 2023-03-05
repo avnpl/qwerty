16200342c8673869fab8a2c3b961e78cbb2cfe8e
avnpl <alvinpaul1311@gmail.com>
2023-03-05 20:01:38 +0530

    ✨ New users can be added using api

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,33 @@
+import { PrismaClient } from '@prisma/client'
 import express from 'express'
 
 const router = express.Router()
+const prisma = new PrismaClient()
 
 router.get('/', (req, res) => {
   res.send({
     message: 'bruh',
+  })
+})
+
+router.post('/api/adduser', async (req, res) => {
+  const { email, name, username, password, bio, interests } = req.body
+
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: password,
+      name: name,
+      username: username,
+      bio: bio,
+      interests: {
+        connect: interests,
+      },
+    },
+  })
+
+  res.send({
+    user,
   })
 })
 

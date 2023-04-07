5c9edd548afc593ff19a001c340484d33667936b
avnpl <alvinpaul1311@gmail.com>
2023-04-07 18:01:34 +0530

    ✨ Added ability to fetch received requests

--- backend/src/matchRoutes.ts
+++ backend/src/matchRoutes.ts
@@ -177,6 +177,95 @@ matchRoutes.get('/api/getnewmatches', async (req, res) => {
   return res.send({ matches })
 })
 
+matchRoutes.get('/api/fetchrequests', async (req, res) => {
+  const username: string = req.body.username
+
+  const matchesOne = await prisma.matches.findMany({
+    where: {
+      userone: {
+        username: username,
+      },
+      mstatus: 'REQUESTED',
+      statone: false,
+    },
+    select: {
+      matchId: true,
+      usertwo: {
+        select: {
+          username: true,
+        },
+      },
+      // statone: true,
+      stattwo: true,
+      mfactor: true,
+      mstatus: true,
+    },
+  })
+  const matchesTwo = await prisma.matches.findMany({
+    where: {
+      usertwo: {
+        username: username,
+      },
+      mstatus: 'MATCHED',
+      stattwo: false,
+    },
+    select: {
+      matchId: true,
+      userone: {
+        select: {
+          username: true,
+        },
+      },
+      statone: true,
+      // stattwo: true,
+      mfactor: true,
+      mstatus: true,
+    },
+  })
+
+  const matches: {
+    matchId: string
+    mfactor: number
+    mstatus: MatchStatus
+    status: boolean
+    username: string
+  }[] = []
+
+  const _matches = [...matchesOne, ...matchesTwo]
+    .sort((a, b) => {
+      return b.mfactor - a.mfactor
+    })
+    .map((item) => {
+      const { matchId, mfactor, mstatus } = item
+      const basic = {
+        matchId,
+        mfactor,
+        mstatus,
+      }
+      if ('usertwo' in item) {
+        const { stattwo, usertwo } = item
+        const obj = {
+          ...basic,
+          status: stattwo,
+          username: usertwo.username,
+          currUser: 1,
+        }
+        matches.push(obj)
+      } else {
+        const { statone, userone } = item
+        const obj = {
+          ...basic,
+          status: statone,
+          username: userone.username,
+          currUser: 2,
+        }
+        matches.push(obj)
+      }
+    })
+
+  return res.send({ matches })
+})
+
 matchRoutes.post('/api/sendrequest', async (req, res) => {
   const parseRes = sendReqBodySchema.safeParse(req.body)
   if (!parseRes.success) {
